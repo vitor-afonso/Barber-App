4fb545f503a2c18a65102d7801e10c333f59032a
Vitor Afonso <vittokm@hotmail.com>
2022-03-03 11:49:14 +0000

    implemented maps tp the footer

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,31 @@ document.addEventListener(
   false
 );
 
+// TO ADD A MAP
+
+function startMap() {
+  const ironhackLSB = {
+  	lat: 38.71164557730083,
+  	lng: -9.124221731921713};
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      zoom: 15,
+      center: ironhackLSB
+    }
+  );
+  // TO ADD A MARKER IN THE MAP
+  const myMarker = new google.maps.Marker({
+    position: {
+      lat: 38.71164557730083,
+      lng: -9.124221731921713
+    },
+    map: map,
+    title: "Ironhack Barber-App Development"
+  });
+}
+
+startMap();
 
 
 
